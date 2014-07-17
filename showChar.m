a43db7b3fc26be0629b08a56fd090945e7440203
Ignacio Tartavull <tartavull@gmail.com>
2014-07-17 11:06:09 -0300

    added dictonary

---
+++ showChar.m
@@ -0,0 +1,4 @@
+%%Plot a single char
+function showChar(charMatrix)
+imshow(charMatrix,'InitialMagnification',1000)
+end
---
+++ showDictionary.m
@@ -0,0 +1,11 @@
+%%Show all the characters
+function showDictionary(dictionary)
+
+for k=1:size(dictionary);
+    subplot(6,8,k),imshow(dictionary{k,2},'InitialMagnification',1000),title(dictionary{k,1});
+end
+end
+
+
+
+
