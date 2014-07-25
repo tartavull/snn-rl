a6414425db0201cedddcb7d2f6d6626758b046a0
Nate Sutton <natemsutton@gmail.com>
2014-07-25 08:44:19 -0400

    Creation of Dirac function
    Dirac function (Dt) calculation function

---
+++ dirac.m
@@ -0,0 +1,15 @@
+function Di = dirac(Dt, t)
+%Dirac function: Calculates Di
+%   Uses the Dirac function as described in the book 'Theoretical 
+%   Neuroscience' on page 405.  The function can be defined in other 
+%   ways but this is one way to define it.  
+%   Dt = delta spike time.  
+%   t = spike time.
+%   The formula below represents the limit of the reciprocal of Dt as
+%   Dt approaches 0.
+
+if (t > -Dt / 2) && (t < Dt / 2)
+    Di = limit(1 / Dt, Dt, 0);
+else
+    Di = 0;
+end
