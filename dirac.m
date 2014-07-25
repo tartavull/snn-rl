c4d167cadb8a05f0ad27d3db506eb0d6bf4594ae
Nate Sutton <natemsutton@gmail.com>
2014-07-25 14:21:38 -0400

    Dirac function (Di) calculation
    Removed the limit function due to matlab compatibility and will look into another method to add it or the effect this will cause

--- dirac.m
+++ dirac.m
@@ -6,11 +6,15 @@ function Di = dirac(Dt, t)
 %   as the limit of a square pulse.
 %   Dt = delta spike time.  
 %   t = spike time.
-%   The formula below represents the limit of the reciprocal of Dt as
-%   Dt approaches 0.
+%   The origional dirac formula represents the limit of the reciprocal 
+%   of Dt as Dt approaches 0.  The limit function is only availible in the
+%   Symbolic Math Toolbox and some other may not have that so I have
+%   excluded it here.
+%   TODO: Now looking into another way to create the limit function or what
+%   effect removing it may cause.
 
-if (t > -Dt / 2) && (t < Dt / 2)
-    Di = limit(1 / Dt, Dt, 0);
+if (t > -Dt / 2) && (t < Dt / 2) && Dt ~= 0
+    Di = 1 / Dt;
 else
     Di = 0;
 end
