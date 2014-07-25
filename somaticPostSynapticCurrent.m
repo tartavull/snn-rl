0a0919a10011e93906a7a41d20cec1d9342f16d2
Nate Sutton <natemsutton@gmail.com>
2014-07-25 09:45:22 -0400

    Somatic Post Synaptic Current (Dpsc) calculation function

---
+++ somaticPostSynapticCurrent.m
@@ -0,0 +1,17 @@
+function Spsc = somaticPostSynapticCurrent(Is, WiMat, DiMat, NTotal)
+% Somatic Post Synaptic Current: Calculates Spsc
+%   Calculates Spsc based on Is, WiMat, DiMat, NTotal
+%   I did not include the matlab sigma function because some contributers
+%   may not have the Symbolic Toolbox and I replaced it with a loop.
+%   Is = Somatic current input
+%   WiMat = Matrix of Weights
+%   DiMat = Matrix of pre-synaptic spike times filtered as dirac pulses
+%   NTotal is the total number of input neurons
+
+sigmaTotal = 0;
+
+for i=1:NTotal
+    sigmaTotal = sigmaTotal + (WiMat(i)*DiMat(1));
+end
+
+Spsc = -Is + sigmaTotal;
