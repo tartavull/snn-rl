0e0a004d1df01ddc4d3a238cfcefe320ca04ebf1
Nate Sutton <natemsutton@gmail.com>
2014-08-01 11:51:24 -0400

    Dendritic Post Synaptic Current (Dpsc) calculation function
    Added rearranging equation to solve for current

--- demos/dendriticPostSynapticCurrent.m
+++ demos/dendriticPostSynapticCurrent.m
@@ -1,9 +1,16 @@
-function Dpsc = dendriticPostSynapticCurrent(Id, Rd, Wi, Di)
+function Dpsc = dendriticPostSynapticCurrent(Rd, Wi, t, tf, stc)
 % Dendritic Post Synaptic Current: Calculates Dpsc
-%   Calculates Dpsc based on Id, Rd, Wi, Di
-%   Id = Dendritic current input
+%   This function uses the total post synaptic current equation
+%   Tau*Derivative[i[t], t] == - i[t] + r*w*DiracDelta[t - tf]
+%   and solves for i (current).  Mathematica was used with the
+%   formula DSolve[\[Tau]*D[i[t], t] == - i[t] + r*w*DiracDelta[t - tf], i, t]
+%   for the result.
+%
+%   Calculates Dpsc based on Rd, Wi, t, tf, stc
 %   Rd = Resistance
 %   Wi = Weight attached to the dendrite
-%   Di = Dirac pulses calculated using the dirac function
+%   t = spike time.
+%   tf = pre-synaptic spike time
+%   stc = Soma time constant
 
-Dpsc = -Id + Rd * Wi * Di;
+Dpsc = log(-t/stc+((log(-t/stc+tf/stc)*Rd*Wi*heaviside(t-tf))/stc))
