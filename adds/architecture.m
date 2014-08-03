7eb827f29b40a6b5d6df60665b02e0e7b2c1c42f
sebacapa <sebastian.caparroz@hotmail.com>
2014-08-02 23:28:05 -0300

    Simulation architecture

--- adds/architecture.m
+++ adds/architecture.m
@@ -7,13 +7,18 @@ timeStep = 0.2; %time step for the simulation is 0.2 ms
 restPotential = 0;
 likV = restPotential * ones(15,1);  % Initial values of voltage
 stimulusIntensity = 20;
-threshold = 10;
 capacitance = 20;
 resistance = 0.8;
 
 %Weights
 weightsDendritic = (0.5 * rand(1) + 0.5) * ones(15,4);
 weightsSomatic = -(0.5 * rand(1) + 0.5) * ones(4,4); % We dont know if this initialization is correct
+weightMaxExcitatory = 1;
+weightMinExcitatory = 0;
+weightMaxInhibitory = 0;
+weightMinInhibitory = -1;
+aPLus = 0.1;
+aMinus = -0.105;
 
 %Time constants
 tauMax = 30;
@@ -28,18 +33,18 @@ currentDendritic = zeros(15,4);
 currentSomatic = zeros(1,4);
 
 %Firings
-firings = zeros(length(Dictionary) * epochs * presentationTime / timeStep;
+firings = zeros(length(Dictionary) * epochs * presentationTime / timeStep , 15);
 
 %Resistences
-
+resistenceDendritic = zeros(15,4);
+resistenceMembrane = 80;
 
 %Voltages
-
+voltagesMembrane = zeros(1,4);
 
 %Firing threshold
-
+firingThreshold = 10;
 
 %Learning rate
-
-
-
+learningRate = 0.1;
+learningRateDecay = 0.05;
