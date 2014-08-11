90f6fcab07744cc96960ee1eaa6329a57999b9f2
Nate Sutton <natemsutton@gmail.com>
2014-08-11 01:54:37 -0400

    Added numberOfSpikesPerChar statistic reporting and shortened description of other stats

--- adds/presentResults.m
+++ adds/presentResults.m
@@ -1,8 +1,9 @@
-function [ output_args ] = presentResults( uniqueSpikePercentageTotal, topVsClosestNtmpTotal, topVsAllNtmpTotal, numberOfChars )
+function [ output_args ] = presentResults( uniqueSpikePercentageTotal, numberOfSpikesPerChar, Dictionary, topVsClosestNtmpTotal, topVsAllNtmpTotal, numberOfChars )
 %Present Results: presents a description of performance results
 %   uniqueSpikePercentage = the average number of spikes produced for each 
 %   Char.  It represents the amount of neurons that uniquely identify with each 
 %   Char.
+%   numberOfSpikesPerChar = Spikes fired for each Char
 %   topVsClosestNtmpTotal = The difference in membrane potential between
 %   the top potential and the one closest to it.  Represents how distinctly
 %   the top responding neuron's potential was compared to the 2cnd highest.
@@ -11,15 +12,21 @@ function [ output_args ] = presentResults( uniqueSpikePercentageTotal, topVsClos
 %   the top responding neuron's potential was compared to the average of the others.
 
 
-fprintf('Average Percentage of Spikes per Char:');
+fprintf('Avg Percent of Spikes per Char:  ');
 fprintf('%d\t', uniqueSpikePercentageTotal/numberOfChars);
 fprintf('\n');
 
-fprintf('Difference in Top Highest Neuron Total Membrane Potenial (Ntmp) and Potential of Closest Ntmp:');
+fprintf('Spikes per Char:  ');
+for letterIndex = 1:size(numberOfSpikesPerChar, 2)
+    fprintf('%s: %d  ', Dictionary{letterIndex}, numberOfSpikesPerChar(1, letterIndex));
+end
+fprintf('\n');
+
+fprintf('topVsClosestNtmp:  ');
 fprintf('%d\t', topVsClosestNtmpTotal/numberOfChars);
 fprintf('\n');
 
-fprintf('Difference in Top Highest Neuron Total Membrane Potenial (Ntmp) and Average Potential of All Other Ntmp:');
+fprintf('topVsAllNtmp:  ');
 fprintf('%d\t', topVsAllNtmpTotal/numberOfChars);
 fprintf('\n');
 
