2e8b564f0feb7ee844487e81d014e0100b5e3121
Ignacio Tartavull <tartavull@gmail.com>
2014-08-25 18:13:40 -0300

    added conection between layers

--- gupta_paper.py
+++ gupta_paper.py
@@ -1,19 +1,44 @@
 import dictionary
-
+import numpy as np
 from brian import *
 
-tau = 20 * msecond        # membrane time constant
-Vt = -50 * mvolt          # spike threshold
-Vr = -60 * mvolt          # reset value
-El = -49 * mvolt          # resting potential (same as the reset)
-psp = 0.5 * mvolt         # postsynaptic potential size
-
-
 spiketimes = dictionary.spikeTimes(dictionaryLongitude=4, spikeInterval=100 * ms, spikesPerChar=3, epochs=100)
-
 LIK = SpikeGeneratorGroup(15, spiketimes)
-ADDS = NeuronGroup(N=4, model='dV/dt = -(V-El)/tau : volt',threshold=Vt, reset=Vr)
 
-C1 = Connection(LIK, ADDS)
 
-run(100 * ms)
+taum = 20 * ms
+taue = 1 * ms
+taui = 10 * ms
+Vt = 10 * mV
+Vr = 0 * mV
+
+eqs = Equations('''
+      dV/dt  = (-V+ge-gi)/taum : volt
+      dge/dt = -ge/taue        : volt
+      dgi/dt = -gi/taui        : volt
+      ''')
+ADDS = NeuronGroup(N=4, model=eqs,threshold=Vt, reset=Vr)
+
+
+exhitatory = Connection(LIK, ADDS , 'ge')
+Wexhitatory = np.random.uniform(0,100,[15,4]) * mV
+exhitatory.connect(LIK,ADDS,Wexhitatory)
+
+inhibitory = Connection(ADDS, ADDS , 'gi')
+Winhibitory = np.random.uniform(0,10,[4,4]) * mV
+inhibitory.connect(ADDS,ADDS,Winhibitory)
+
+
+Mv = StateMonitor(ADDS, 'V', record=True)
+Mge = StateMonitor(ADDS, 'ge', record=True)
+Mgi = StateMonitor(ADDS, 'gi', record=True)
+
+run(1000 * ms)
+
+figure()
+subplot(211)
+plot(Mv.times / ms, Mv[0] / mV)
+subplot(212)
+plot(Mge.times / ms, Mge[0] / mV)
+plot(Mgi.times / ms, Mgi[0] / mV)
+show()
