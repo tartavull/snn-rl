2bde26f6dee202be0543a4e28802342a3e216395
Nate Sutton <natemsutton@gmail.com>
2014-08-25 01:45:19 -0400

    Port of Matlab code to Octave

--- auxiliar/CombinedMonitors.m
+++ auxiliar/CombinedMonitors.m
@@ -1,41 +1,35 @@
-classdef CombinedMonitors < handle
+function obj = CombinedMonitors()
     %COMBINEDMONITORS Use several monitors to plot the activity of a neural
     %network
-    
-    properties
+
         handle = figure();
         monitors = struct();
         enabled = true;
-    end
-    
-    methods
-        function obj = CombinedMonitors(enabled)
-            obj.enabled = enabled;
-            if (enabled)
-                
-                %Construct input Monitor
-                obj.monitors.meshMonitor = Monitor;
-                obj.monitors.meshMonitor.setSubPlot(obj.handle,3,2,2);
-                %Construct Char Monitor
-                obj.monitors.charMonitor = Monitor;
-                obj.monitors.charMonitor.setSubPlot(obj.handle,3,2,1);
-                obj.monitors.charMonitor.setPlotType('char');
-                %Construct LIK Spike Monitor
-                obj.monitors.spikeMonitor = Monitor;
-                obj.monitors.spikeMonitor.setSubPlot(obj.handle,3,2,[3 4]);
-                obj.monitors.spikeMonitor.setPlotType('lines3d');
-                %Construct adds Spike Monitor
-                obj.monitors.addsMonitor = Monitor;
-                obj.monitors.addsMonitor.setSubPlot(obj.handle,3,2,[5 6]);
-                obj.monitors.addsMonitor.setPlotType('lines3d');
-            end
+
+        function obj = CombinedMonitors()
             
-        end
+            %Construct input Monitor
+            obj.monitors.meshMonitor = Monitor;
+            obj.monitors.meshMonitor.setSubPlot(obj.handle,3,2,2);
+            %Construct Char Monitor
+            obj.monitors.charMonitor = Monitor;
+            obj.monitors.charMonitor.setSubPlot(obj.handle,3,2,1);
+            obj.monitors.charMonitor.setPlotType('char');
+            %Construct LIK Spike Monitor
+            obj.monitors.spikeMonitor = Monitor;
+            obj.monitors.spikeMonitor.setSubPlot(obj.handle,3,2,[3 4]);
+            obj.monitors.spikeMonitor.setPlotType('lines3d');
+            %Construct adds Spike Monitor
+            obj.monitors.addsMonitor = Monitor;
+            obj.monitors.addsMonitor.setSubPlot(obj.handle,3,2,[5 6]);
+            obj.monitors.addsMonitor.setPlotType('lines3d');
+            
+        endfunction
         
         function record_EpochLoop(obj)
             if(obj.enabled)
-            end
-        end
+            endif
+        endfunction
         
         function record_DictionaryLoop(obj,time,charMatrix,input)
             if(obj.enabled)
@@ -44,8 +38,8 @@ classdef CombinedMonitors < handle
                 
                 obj.monitors.charMonitor.plot();
                 obj.monitors.meshMonitor.plot();
-            end
-        end
+            endif
+        endfunction
         
         function record_StepLoop(obj,time,likV,voltagesMembrane)
             if(obj.enabled)
@@ -54,9 +48,7 @@ classdef CombinedMonitors < handle
                 
                 obj.monitors.spikeMonitor.plot();
                 obj.monitors.addsMonitor.plot();
-            end
-        end
-    end
+            endif
+        endfunction
     
-end
-
+endfunction
