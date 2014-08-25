06f34e0681205b59258095c3643dbd1106fb40be
Nate Sutton <natemsutton@gmail.com>
2014-08-25 01:55:55 -0400

    Port of Matlab code to Octave

--- datasets/showDictionary.m
+++ datasets/showDictionary.m
@@ -3,8 +3,8 @@ function showDictionary(dictionary)
 
 for k=1:size(dictionary);
     subplot(6,8,k),imshow(dictionary{k,2},'InitialMagnification',1000),title(dictionary{k,1});
-end
-end
+endfor
+endfunction
 
 
 
