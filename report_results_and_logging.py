00d1eab54eadfda340b5af9a1f4c2b9cacc6cd53
Nate Sutton <natemsutton@gmail.com>
2014-09-03 05:03:15 -0400

    Results reporting and logging
    Due to both methods using many of the same arguments passed to them and SpikeNumberInEpoch variable a class was made that allows sharing of those resources with the methods.

---
+++ report_results_and_logging.py
@@ -0,0 +1,41 @@
+class report_results_and_logging:
+	def __init__(self, dictionaryLongitude, epochsToPrint, M, Mv, epochIndex, spikeIntervalUnformatted, dictionary):
+		self.epochsToPrint = epochsToPrint
+		self.dictionaryLongitude = dictionaryLongitude
+		self.M = M
+		self.Mv = Mv
+		self.epochIndex = epochIndex
+		self.spikeIntervalUnformatted = spikeIntervalUnformatted
+		self.dictionary = dictionary
+
+		print 'Epoch number:', self.epochIndex
+		SpikeNumberInEpoch = [0] * self.dictionaryLongitude
+
+		for NeuronNumber in range(self.dictionaryLongitude):
+			# Spikes per char counter
+			for spikeOccurenceTime in self.M[NeuronNumber]:
+				if (spikeOccurenceTime >= (self.epochIndex * self.spikeIntervalUnformatted) and spikeOccurenceTime < ((self.epochIndex * self.spikeIntervalUnformatted) + (self.spikeIntervalUnformatted - .001))):
+					SpikeNumberInEpoch[NeuronNumber] = SpikeNumberInEpoch[NeuronNumber] + 1
+
+		self.SpikeNumberInEpoch = SpikeNumberInEpoch
+
+	def presenter(self):				
+		print ('Avg Percent of Spikes for the Char: ')
+		print (sum(self.SpikeNumberInEpoch)/self.dictionaryLongitude)
+		print ('Spikes for the Char: ')
+		for NeuronNumber in range(self.dictionaryLongitude):
+			print self.dictionary.dictionary[NeuronNumber][0], ':', self.SpikeNumberInEpoch[NeuronNumber], ' ',
+		print (' ')
+		SortedMvForEpoch = []
+		for neuronIndex in range(self.dictionaryLongitude):
+			SortedMvForEpoch.append(sum(self.Mv[neuronIndex][(self.epochIndex*15):(15+(self.epochIndex*15))]))
+		SortedMvForEpoch = sorted(SortedMvForEpoch)
+		print ('topVsClosestNtmp: ')
+		print SortedMvForEpoch[3]-SortedMvForEpoch[2]
+		print ('topVsAllNtmp: ')
+		print SortedMvForEpoch[3]-(sum(SortedMvForEpoch[0:(self.dictionaryLongitude-1)])/(self.dictionaryLongitude-1))
+
+	def logger(self, outputFile):
+		for neuronIndex in range(self.dictionaryLongitude):
+			outputStatement = [str(self.dictionary.dictionary[neuronIndex][0]), ' ', str(self.epochIndex), ' ', str(sum(self.Mv[neuronIndex][(self.epochIndex*15):(15+(self.epochIndex*15))])), ' ', str(self.SpikeNumberInEpoch[neuronIndex]), '\n']
+			outputFile.writelines(outputStatement)
