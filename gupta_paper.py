7d56087d27b8ced9ba2efb17b3d29f6ec6df8e7c
Nate Sutton <natemsutton@gmail.com>
2014-09-03 05:00:19 -0400

    Added results reporting display and also the logger
    To accomidate how Brian's system works I created an alternation on the results reporting and logger.  The alteration reports on the one char input per epoch instead of four chars input per epoch as matlab code used.

--- gupta_paper.py
+++ gupta_paper.py
@@ -1,14 +1,24 @@
-import dictionary
+from dictionary import *
+from report_results_and_logging import *
 import numpy as np
 from brian import *
 
 epochs = 100 
-spikeInterval = 100 * ms
+spikeMiliseconds = 100
+spikeInterval = spikeMiliseconds * ms
+spikeIntervalUnformatted = spikeMiliseconds * .001
 dictionaryLongitude = 4
 spikesPerChar=3
 totalTime = epochs * spikesPerChar * dictionaryLongitude * spikeInterval 
+epochsToPrint = [0,1,2,25,50,75]
+presentResults = True
+logger = True
 
+dictionary = dictionary()
 spiketimes = dictionary.spikeTimes(dictionaryLongitude, spikeInterval, spikesPerChar, epochs)
+print ('spikeTimes2:')
+print (spiketimes)
+print (spiketimes.shape)
 LIK = SpikeGeneratorGroup(15, spiketimes)
 
 
@@ -19,10 +29,10 @@ Vt = 5 * mV
 Vr = 0 * mV
 
 eqs = Equations('''
-      dV/dt  = (-V+ge-gi)/taum : volt
-      dge/dt = -ge/taue        : volt
-      dgi/dt = -gi/taui        : volt
-      ''')
+	  dV/dt  = (-V+ge-gi)/taum : volt
+	  dge/dt = -ge/taue        : volt
+	  dgi/dt = -gi/taui        : volt
+	  ''')
 ADDS = NeuronGroup(N=4, model=eqs,threshold=Vt, reset=Vr)
 
 
@@ -49,6 +59,19 @@ Mge = StateMonitor(ADDS, 'ge', record=True)
 Mgi = StateMonitor(ADDS, 'gi', record=True)
 
 run(10000*ms,threads=2, report='text')
+
+# Present results and logging
+if presentResults == True:
+	for epochIndex in epochsToPrint:
+		reportResultsAndLogging = report_results_and_logging(dictionaryLongitude, epochsToPrint, M, Mv, epochIndex, spikeIntervalUnformatted, dictionary)
+		reportResultsAndLogging.presenter()	
+
+if logger == True:
+	outputFile = open('snnResults.txt', 'w')	
+	for epochIndex in range(epochs):
+		reportResultsAndLogging = report_results_and_logging(dictionaryLongitude, epochsToPrint, M, Mv, epochIndex, spikeIntervalUnformatted, dictionary)
+		reportResultsAndLogging.logger(outputFile)
+	outputFile.close()
 
 neuronToPlot = 1
 subplot(211)
