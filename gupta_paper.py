c103e97a970aca764de41d7c934b29f53fb4947f
Nate Sutton <natemsutton@gmail.com>
2014-09-06 08:20:52 -0400

    Moved some parameters to architecture.py

--- gupta_paper.py
+++ gupta_paper.py
@@ -1,32 +1,8 @@
-from dictionary import *
-from report_results_and_logging import *
-import numpy as np
-from brian import *
-
-epochs = 100 
-spikeMiliseconds = 100
-spikeInterval = spikeMiliseconds * ms
-spikeIntervalUnformatted = spikeMiliseconds * .001
-dictionaryLongitude = 4
-spikesPerChar=3
-totalTime = epochs * spikesPerChar * dictionaryLongitude * spikeInterval 
-epochsToPrint = [0,1,2,25,50,75]
-presentResults = True
-logger = True
+from architecture import *
 
 dictionary = dictionary()
 spiketimes = dictionary.spikeTimes(dictionaryLongitude, spikeInterval, spikesPerChar, epochs)
-print ('spikeTimes2:')
-print (spiketimes)
-print (spiketimes.shape)
 LIK = SpikeGeneratorGroup(15, spiketimes)
-
-
-taum = 20 * ms
-taue = 1 * ms
-taui = 10 * ms
-Vt = 5 * mV
-Vr = 0 * mV
 
 eqs = Equations('''
 	  dV/dt  = (-V+ge-gi)/taum : volt
@@ -34,7 +10,6 @@ eqs = Equations('''
 	  dgi/dt = -gi/taui        : volt
 	  ''')
 ADDS = NeuronGroup(N=4, model=eqs,threshold=Vt, reset=Vr)
-
 
 exhitatory = Connection(LIK, ADDS , 'ge',delay=10*ms,structure='dense')
 Wexhitatory = np.random.uniform(10,50,[15,4]) * mV
