3d822b3448c8526e2eaa2c801660457d3f52b22e
Nate Sutton <natemsutton@gmail.com>
2014-09-12 01:06:41 -0400

    Corrected reporting to use 100ms per epoch.  Changed totalMembraneMv to totalPositiveMembraneMv.
    Incorrect timescale was found to be used previously in results reporting and that was corrected.  Also, in this Brian model it was found that membrane potential can be negative at times which creates challenges when reporting total membrane potential.  The total membrane potential was based on the result in figure 7 in the gupta and long paper: http://i.imgur.com/ZJAYptd.jpg .  To still capture some of the meaning in this model only the positive Mv are reported for this stat.  An issue is that in some neuron epochs that never fire a spike their total Mv is greater than those that fire spikes it seems to me due to no depolarization phase, that in mind the stat still has meaning but I am looking into a better one.

--- report_results_and_logging.py
+++ report_results_and_logging.py
@@ -1,5 +1,5 @@
 class report_results_and_logging:
-	def __init__(self, dictionaryLongitude, epochsToPrint, M, Mv, epochIndex, spikeIntervalUnformatted, dictionary):
+	def __init__(self, dictionaryLongitude, epochsToPrint, M, Mv, epochIndex, spikeIntervalUnformatted, dictionary, epochMsDuration):
 		self.epochsToPrint = epochsToPrint
 		self.dictionaryLongitude = dictionaryLongitude
 		self.M = M
@@ -7,6 +7,7 @@ class report_results_and_logging:
 		self.epochIndex = epochIndex
 		self.spikeIntervalUnformatted = spikeIntervalUnformatted
 		self.dictionary = dictionary
+		self.epochMsDuration = epochMsDuration
 
 		print 'Epoch number:', self.epochIndex
 		SpikeNumberInEpoch = [0] * self.dictionaryLongitude
@@ -28,7 +29,7 @@ class report_results_and_logging:
 		print (' ')
 		SortedMvForEpoch = []
 		for neuronIndex in range(self.dictionaryLongitude):
-			SortedMvForEpoch.append(sum(self.Mv[neuronIndex][(self.epochIndex*15):(15+(self.epochIndex*15))]))
+			SortedMvForEpoch.append(sum(self.Mv[neuronIndex][(self.epochIndex*self.epochMsDuration):(self.epochMsDuration+(self.epochIndex*self.epochMsDuration))]))
 		SortedMvForEpoch = sorted(SortedMvForEpoch)
 		print ('topVsClosestNtmp: ')
 		print SortedMvForEpoch[3]-SortedMvForEpoch[2]
@@ -36,10 +37,15 @@ class report_results_and_logging:
 		print SortedMvForEpoch[3]-(sum(SortedMvForEpoch[0:(self.dictionaryLongitude-1)])/(self.dictionaryLongitude-1))
 
 	def logger(self, outputFile):
-		# The output represents: char presented | epoch number | total neuron membrane voltages in epoch | if neuron had a spike
+		# The output represents: char presented | epoch number | total positive neuron membrane voltages in epoch | if neuron had a spike
 		if self.epochIndex == 0:
-			outputStatement = ['Char\tEpoch\tMembraneVoltage\tSpike\n']
+			outputStatement = ['Char\tEpoch\tPositiveMembraneVoltage\tSpike\n']
 			outputFile.writelines(outputStatement)
 		for neuronIndex in range(self.dictionaryLongitude):
-			outputStatement = [str(self.dictionary.dictionary[neuronIndex][0]), '\t', str(self.epochIndex), '\t', str(sum(self.Mv[neuronIndex][(self.epochIndex*15):(15+(self.epochIndex*15))])), '\t', str(self.SpikeNumberInEpoch[neuronIndex]), '\n']
+			priorMvInEpoch = 0
+			totalPositiveMembraneMv = 0
+			for MvInEpoch in self.Mv[neuronIndex][(self.epochIndex*self.epochMsDuration):(self.epochMsDuration+(self.epochIndex*self.epochMsDuration))]:
+				if MvInEpoch > 0:
+					totalPositiveMembraneMv = totalPositiveMembraneMv + MvInEpoch
+			outputStatement = [str(self.dictionary.dictionary[neuronIndex][0]), '\t', str(self.epochIndex), '\t', str(totalPositiveMembraneMv), '\t', str(self.SpikeNumberInEpoch[neuronIndex]), '\n']
 			outputFile.writelines(outputStatement)
