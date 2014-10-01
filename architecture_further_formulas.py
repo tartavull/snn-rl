2d0dffdc935b0233b75bdf8b14ea49ac3de39ea7
Nate Sutton <natemsutton@gmail.com>
2014-10-01 17:25:35 -0400

    Added further variables and updates
    Added content to accommodate gupta_paper_further_formulas.py file update

--- architecture_further_formulas.py
+++ architecture_further_formulas.py
@@ -23,17 +23,30 @@ Vt = 5 * mV
 Vr = 0 * mV
 
 Rm = 80
+## added tauS constant below ##
+tauS = 2
 tauMax = 30
 tauMin = 2
-tauM = 30 * ms # appears different than taum above due to the article specifying this as 30
+#tauM = 30 * ms # appears different than taum above due to the article specifying this as 30
+tauM = 30 # removed ms for compatibility
 
 SimulationDuration = 10000
 epochMsDuration = (SimulationDuration / epochs) * 10 # Times 10 is to adjust to Ms scale
 
 numberOfPixels = 15
-neuronFiringThreshold = 10 * mV
+#neuronFiringThreshold = 10 * mV
+neuronFiringThreshold = 10 # removed mV for compatibility
 W = np.random.uniform(0.5,1.0,[15,4]) # Initial weights
-R = [[None] * dictionaryLongitude for i in range(numberOfPixels)] # Initial Resistance Values
-Id = [[None] * dictionaryLongitude for i in range(numberOfPixels)] # Initial Dendritic Post Synaptic Current
-tauD = [[None] * dictionaryLongitude for i in range(numberOfPixels)] # Initial tauD
+# none to 1 below
+numberOfNeurons = 4
+R = [[1] * dictionaryLongitude for i in range(numberOfPixels)] # Initial Resistance Values
+## Added 1 init below but I don't really know what the right init value is
+Id = [[1] * dictionaryLongitude for i in range(numberOfPixels)] # Initial Dendritic Post Synaptic Current
+## Added 1 init below but I don't really know what the right init value is
+tauD = [[1] * dictionaryLongitude for i in range(numberOfPixels)] # Initial tauD
 tF = [[None] * dictionaryLongitude for i in range(numberOfPixels)] # Initial pre-synaptic spike times
+
+Is = [1] * dictionaryLongitude
+Um = [1] * dictionaryLongitude
+
+Ureset = -.001
