9b5500efa5f2fab00add685c502eae4dbcf29791
Nate Sutton <natemsutton@gmail.com>
2014-10-11 22:46:25 -0400

    The symbolic diff equ output soma membrane potential is now integrated into brian's code
    The Um output is applied directly as each neuron's voltage (V) for each Ms.  The network_operation achieves this.  The computations of the symbolic diff equ are now accessible for plotting and all of brian's other functionality.  A new output display has been added named displayAllNeuronMemPotentials which shows the changes over time of Um.  The original output display of 'total positive neuron membrane voltages' was not capturing the output Mv well because they are largely negative now for some reason.  Next work is to understand how well the calculations are performing and try to have the model learn the weights better.  Also, I will perhaps add more variables than just 'V' for plotting and other things since 'ge' and 'gi' were removed due to different formulas being used.

--- gupta_paper_further_formulas.py
+++ gupta_paper_further_formulas.py
@@ -1,157 +1,160 @@
 from architecture_further_formulas import *
-from weightConnection import *
 
-dictionary = dictionary()
-spiketimes = dictionary.spikeTimes(dictionaryLongitude, spikeInterval, spikesPerChar, epochs)
-LIK = SpikeGeneratorGroup(15, spiketimes)
+class gupta_paper:
+	dictionary = dictionary()
+	spiketimes = dictionary.spikeTimes(dictionaryLongitude, spikeInterval, spikesPerChar, epochs)
+	LIK = SpikeGeneratorGroup(15, spiketimes)
 
-class UmUpdate:
 	neuronIndex = 0
+	neuronCounter = -1
 	t = 1
-
-	def returnUm(self):
-		neuronIndex = self.neuronIndex
-		t = self.t
-
-		# Weight loop
-		for WIndex in range(len(W)):
-			if abs(W[WIndex][neuronIndex]) <= 1:
-				tauD[WIndex][neuronIndex] = tauMax - abs(W[WIndex][neuronIndex])*(tauMax-tauMin)
-
-		#Resistance loop
-		for RIndex in range(len(R)):
-			R[RIndex][neuronIndex] = ((tauD[RIndex][neuronIndex]*neuronFiringThreshold) / Rm) * ((tauM / tauD[RIndex][neuronIndex]) ** (tauM / tauM - tauD[RIndex][neuronIndex]))
-
-		#Dedritic total post-synaptic current
-		# Solving for Id in the formula in the article yeilded the below equation
-		# -Id**2/(2*Td) + Id*Rd*W/Tfd
-		# That is implemented below
-		e = math.e
+	def run_model(self):
 		
-		for IdIndex in range(len(Id)):
-			tauDen = tauD[IdIndex][neuronIndex]
-			r = R[IdIndex][neuronIndex]
-			w = W[IdIndex][neuronIndex]
-			tPreSyn = spiketimes[IdIndex + (neuronIndex * len(Id))][0]
-			Id2 = Id[IdIndex][neuronIndex]
-			Dt = t - tPreSyn
-			DiracFun = 1/Dt
+		dictionary = self.dictionary
+		spiketimes = self.spiketimes
 
-			# dirac test
-			# t in dirac forumula means curent time or last spike time?
-			if (t > -(Dt/2) and t < (Dt/2)):
-				Id[IdIndex][neuronIndex] = ((-Id2**2)/(2*tauDen))+((Id2*r*w)*DiracFun)
-			else:
-				DiracFun = 0
-				Id[IdIndex][neuronIndex] = ((-Id2**2)/(2*tauDen))+((Id2*r*w)*DiracFun)
+		eqs = Equations('''
+		      V : volt
+			  ''')
 
-		### Synapse directly to soma ###
-		# Solving for Id in the formula in the article yeilded the below equation
-		# -Is_1**2/(2*Ts_1) + (Is_1*DiracWeightedSum)/Ts_1
-		# That is implemented below
-		# To calculate the DiracWeightedSum the spike times with the dirac function applied are multipled by the synapse weight 
-		# and summed then divided by the number of synapses for the neuron 
-		DiracWeightedSum = 0
-		for DiracIndex in range(len(Is)):
-			tPreSyn = spiketimes[DiracIndex + (neuronIndex * len(Is))][0]
-			Dt = t - tPreSyn
-			DiracFun = 1/Dt
-			# dirac test  # TODO: not sure dirac is implemented correctly here
-			if (t > -(Dt/2) and t < (Dt/2)):
-				DiracFunctionWithSpikeTimes = DiracFun # (-t+tPreSyn)/tauDen
-			else:
-				DiracFunctionWithSpikeTimes = 0
-			DiracWeightedSum = DiracWeightedSum + W[WIndex][neuronIndex] * DiracFunctionWithSpikeTimes
-		DiracWeightedSum = DiracWeightedSum / len(Id)
+		ADDS = NeuronGroup(N=4, model=eqs,threshold=Vt, reset=Vr)
 
-		Is2 = Is[neuronIndex]
+		def returnUm(self):
+		
+			dictionary = self.dictionary
+			neuronIndex = self.neuronIndex
+			t = self.t
+			spiketimes = self.spiketimes
+			
+			# Weight loop
+			for WIndex in range(len(W)):
+				if abs(W[WIndex][neuronIndex]) <= 1:
+					tauD[WIndex][neuronIndex] = tauMax - abs(W[WIndex][neuronIndex])*(tauMax-tauMin)
 
-		Is[neuronIndex] = (-Is2**2)/(2*tauS) + ((Is2*DiracWeightedSum)/tauS)
+			#Resistance loop
+			for RIndex in range(len(R)):
+				R[RIndex][neuronIndex] = ((tauD[RIndex][neuronIndex]*neuronFiringThreshold) / Rm) * ((tauM / tauD[RIndex][neuronIndex]) ** (tauM / tauM - tauD[RIndex][neuronIndex]))
 
-		### Soma membrane potential ###
-		# Solving for Um in the formula in the article yeilded the below equation
-		## -Um**2/(2*Tm) + Um*(Rm*SummedDendriteGroupX + Rm*SynapseToSomaX)/Tm
-		# That is implemented below
-		Um2 = Um[neuronIndex]
-		SummedDendriteGroup = sum(Id[ 0:len(Id) ][neuronIndex])
-		SynapseToSoma = Is[neuronIndex]
+			#Dedritic total post-synaptic current
+			# Solving for Id in the formula in the article yeilded the below equation
+			# -Id**2/(2*Td) + Id*Rd*W/Tfd
+			# That is implemented below
+			e = math.e
+			
+			for IdIndex in range(len(Id)):
+				tauDen = tauD[IdIndex][neuronIndex]
+				r = R[IdIndex][neuronIndex]
+				w = W[IdIndex][neuronIndex]
+				tPreSyn = spiketimes[IdIndex + (neuronIndex * len(Id))][0]
+				Id2 = Id[IdIndex][neuronIndex]
+				Dt = t - tPreSyn
+				DiracFun = 1/Dt
 
-		Um[neuronIndex] = -Um2**2/(2*tauM) + Um2*(Rm*SummedDendriteGroup + Rm*SynapseToSoma)/tauM
+				# dirac test
+				# t in dirac forumula means curent time or last spike time?
+				if (t > -(Dt/2) and t < (Dt/2)):
+					Id[IdIndex][neuronIndex] = ((-Id2**2)/(2*tauDen))+((Id2*r*w)*DiracFun)
+				else:
+					DiracFun = 0
+					Id[IdIndex][neuronIndex] = ((-Id2**2)/(2*tauDen))+((Id2*r*w)*DiracFun)
 
-		if Um[neuronIndex] == 0: Um[neuronIndex] = Ureset
+			### Synapse directly to soma ###
+			# Solving for Id in the formula in the article yeilded the below equation
+			# -Is_1**2/(2*Ts_1) + (Is_1*DiracWeightedSum)/Ts_1
+			# That is implemented below
+			# To calculate the DiracWeightedSum the spike times with the dirac function applied are multipled by the synapse weight 
+			# and summed then divided by the number of synapses for the neuron 
+			DiracWeightedSum = 0
+			for DiracIndex in range(len(Is)):
+				tPreSyn = spiketimes[DiracIndex + (neuronIndex * len(Is))][0]
+				Dt = t - tPreSyn
+				DiracFun = 1/Dt
+				# dirac test  # TODO: not sure dirac is implemented correctly here
+				if (t > -(Dt/2) and t < (Dt/2)):
+					DiracFunctionWithSpikeTimes = DiracFun # (-t+tPreSyn)/tauDen
+				else:
+					DiracFunctionWithSpikeTimes = 0
+				DiracWeightedSum = DiracWeightedSum + W[WIndex][neuronIndex] * DiracFunctionWithSpikeTimes
+			DiracWeightedSum = DiracWeightedSum / len(Id)
 
-		### After all neurons are cycled through the time is iterated by one
-		neuronIndex = neuronIndex + 1
-		if neuronIndex == 4: 
-			neuronIndex = 0
-			t = t + 1
-		self.neuronIndex = neuronIndex
-		self.t = t
+			Is2 = Is[neuronIndex]
 
-		return Um[neuronIndex] * mV;
+			Is[neuronIndex] = (-Is2**2)/(2*tauS) + ((Is2*DiracWeightedSum)/tauS)
 
-UmComputation = UmUpdate()
+			### Soma membrane potential ###
+			# Solving for Um in the formula in the article yeilded the below equation
+			## -Um**2/(2*Tm) + Um*(Rm*SummedDendriteGroupX + Rm*SynapseToSomaX)/Tm
+			# That is implemented below
+			Um2 = Um[neuronIndex]
+			SummedDendriteGroup = sum(Id[ 0:len(Id) ][neuronIndex])
+			SynapseToSoma = Is[neuronIndex]
 
-### 100 ms simulation ###
-for i in range(4000):
-	UmResult = UmComputation.returnUm()
-	print 'Time:', UmComputation.t, '\tNeuron Index:', UmComputation.neuronIndex, '\tUmResult:', UmResult
+			Um[neuronIndex] = -Um2**2/(2*tauM) + Um2*(Rm*SummedDendriteGroup + Rm*SynapseToSoma)/tauM
 
-eqs = Equations('''
-	  dV/dt  = (-V+ge-gi)/taum : volt
-	  dge/dt = -ge/taue        : volt
-	  dgi/dt = -gi/taui        : volt
-	  ''')
-ADDS = NeuronGroup(N=4, model=eqs,threshold=Vt, reset=Vr)
+			if Um[neuronIndex] == 0: Um[neuronIndex] = Ureset
 
-exhitatory2 = weightConnection(LIK, ADDS , 'ge',delay=10*ms,structure='dense')
-exhitatory = Connection(LIK, ADDS , 'ge',delay=10*ms,structure='dense')
-Wexhitatory = np.random.uniform(10,50,[15,4]) * mV
-exhitatory2.connect(LIK,ADDS,Wexhitatory)
-Ap = 1 * mV
-Am = 1 * mV
-stdp=ExponentialSTDP(exhitatory2,taue,taum,Ap,Am,wmax=50 * mV,interactions='all',update='additive')
+			### After all neurons are cycled through the time is iterated by one
+			neuronIndex = neuronIndex + 1
+			if neuronIndex == 4: 
+				neuronIndex = 0
+				t = t + 1
+			self.neuronIndex = neuronIndex
+			self.t = t
 
-inhibitory = Connection(ADDS, ADDS , 'gi',delay=5*ms,structure='dense')
-#Connect adds layer via lateral inhibitory connections
-#the diagonal should be 0 to not auto-inhibate
-Winhibitory = np.random.uniform(0,5,[4,4]) * mV
-diagonal = np.diag_indices(Winhibitory.shape[0])
-Winhibitory[diagonal] = 0;
+			return Um[neuronIndex] * mV;	
 
-inhibitory.connect(ADDS,ADDS,Winhibitory)
+		# This network_operation runs the membrane potential calculation function for every milisecond that occurs.
+		# The Um output is saved directly to the ADDS V (voltage) records for use with Brian's code.
+		@network_operation
+		def myoperation():
+			self.neuronCounter = self.neuronCounter + 1
+			if self.neuronCounter == 4:
+				self.neuronCounter = 0
 
-M = SpikeMonitor(ADDS)
-Mv = StateMonitor(ADDS, 'V', record=True)
-Mge = StateMonitor(ADDS, 'ge', record=True)
-Mgi = StateMonitor(ADDS, 'gi', record=True)
+			ADDS[self.neuronCounter][0].V = returnUm(self)
 
-run(10000*ms,threads=2, report='text')
+		M = SpikeMonitor(ADDS)
+		Mv = StateMonitor(ADDS, 'V', record=True)
 
-# Present results and logging
-if presentResults == True:
-	for epochIndex in epochsToPrint:
-		reportResultsAndLogging = report_results_and_logging(dictionaryLongitude, epochsToPrint, M, Mv, epochIndex, spikeIntervalUnformatted, dictionary, epochMsDuration)
-		reportResultsAndLogging.presenter()	
+		totalRunTime = 1000
 
-if logger == True:
-	outputFile = open('snnResults.txt', 'w')	
-	for epochIndex in range(epochs):
-		reportResultsAndLogging = report_results_and_logging(dictionaryLongitude, epochsToPrint, M, Mv, epochIndex, spikeIntervalUnformatted, dictionary, epochMsDuration)
-		reportResultsAndLogging.logger(outputFile)
-	outputFile.close()
+		run(totalRunTime*ms,threads=2, report='text')
 
-neuronToPlot = 1
-subplot(211)
-raster_plot(M, title='The gupta network', newfigure=False)
-subplot(223)
-plot(Mv.times / ms, Mv[neuronToPlot] / mV)
-xlabel('Time (ms)')
-ylabel('V (mV)')
-subplot(224)
-plot(Mge.times / ms, Mge[neuronToPlot] / mV)
-plot(Mgi.times / ms, Mgi[neuronToPlot] / mV)
-xlabel('Time (ms)')
-ylabel('ge and gi (mV)')
-legend(('ge', 'gi'), 'upper right')
-show()
+		# Present results and logging
+		if presentResults == True:
+			for epochIndex in epochsToPrint:
+				reportResultsAndLogging = report_results_and_logging(dictionaryLongitude, epochsToPrint, M, Mv, epochIndex, spikeIntervalUnformatted, dictionary, epochMsDuration)
+				reportResultsAndLogging.presenter()	
+
+		# Show all membrane potentials
+		# if statement with epochIndex below is to filter only epochs actaully included in a run
+		if displayAllNeuronMemPotentials == True:
+			for neuronIdentity in range(dictionaryLongitude):
+				for epochIndex in epochsToPrint:
+					if epochIndex <= totalRunTime/100:
+						for epochMs in range(epochMsDuration):
+							print 'neuron\t', neuronIdentity, '\tepochIndex\t', epochIndex, '\tepochMs\t', epochMs, '\tMembranePotential\t', Mv[neuronIdentity][(epochIndex*epochMsDuration)+epochMs]
+
+		if logger == True:
+			outputFile = open('snnResults.txt', 'w')	
+			for epochIndex in range(epochs):
+				reportResultsAndLogging = report_results_and_logging(dictionaryLongitude, epochsToPrint, M, Mv, epochIndex, spikeIntervalUnformatted, dictionary, epochMsDuration)
+				reportResultsAndLogging.logger(outputFile)
+			outputFile.close()
+
+		neuronToPlot = 1
+		subplot(211)
+		raster_plot(M, title='The gupta network', newfigure=False)
+		subplot(223)
+		plot(Mv.times / ms, Mv[neuronToPlot] / mV)
+		xlabel('Time (ms)')
+		ylabel('V (mV)')
+		show()
+
+	def __init__(self):
+		self.run_model()
+
+def main():
+	run_gupta_paper = gupta_paper()
+
+if  __name__ =='__main__':main()
