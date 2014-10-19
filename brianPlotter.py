41b5d5a02b39bbb8ea97bac750927905e018c51c
Ignacio Tartavull <tartavull@gmail.com>
2014-10-19 17:05:32 -0300

    improved plotting speed removing mpld3 dependency

--- brianPlotter.py
+++ brianPlotter.py
@@ -1,4 +1,6 @@
 import h5py
+from bisect import bisect_left
+import matplotlib.pyplot as plt
 import numpy as np
 
 class brianPlotter:
@@ -18,6 +20,7 @@ class brianPlotter:
                 self.minMaxDownsampling(dataName, level , step)
                 level = level * step
 
+        print dataName + ' sucessfully saved';
     def __del__ (self):
         self.hdf.close()
         
@@ -37,24 +40,82 @@ class brianPlotter:
         level = int(baseLevel)*step
         self.grp.create_dataset(str(level), data=downsample)
     
-    def plot(self, group,  level ):
+    def plotLine(self, group, start, end):
 
-        data = self.hdf['/'+group+'/'+str(level)][0,:]
-        time = range(0,data.shape[0]*level,level)
-        plt.plot(time,data)
+        data = self.getDownsampleData(group, start, end)
+        length = self.getGroupLength(group)
+        
+        nrows = data.shape[1]
+     
+        fig, axes = plt.subplots(ncols=1, nrows=nrows, figsize=(16,nrows*2.5),sharey=True,sharex = True)
+        fig.suptitle(group, fontsize=20)
+
+        for index in range(nrows):     
+            legend = 'neuron '+str(index)
+            if (len(data.shape) == 3):
+                legend = []
+                for l in range(data.shape[2]):
+                    legend.append('Synapse '+str(index)+ '-'+str(l))
+            
+            axes[index].plot(data[:,index])
+            axes[index].legend(legend,framealpha=0.5)
+
+    def plotScatter(self, start, end):
+        
+        spikes = self.getData('spikes',1)
+
+        start =  self.binary_search(spikes[:,1], start)
+        end = self.binary_search(spikes[:,1], end)
+
+        fig, ax = plt.subplots(subplot_kw=dict(axisbg='#EEEEEE'),figsize=(16,4))
+        points = plt.scatter(spikes[start:end,1],spikes[start:end,0], s=100)
+        plt.yticks(np.arange(0,4, 1.0))
+        plt.ylabel("Neuron number")
+        plt.xlabel("Time(seconds)")
+        plt.grid(True)
 
     def getData(self, group,  level ):
 
         return self.hdf['/'+group+'/'+str(level)]
 
-    def getDownsampleData(self, group, start, end):
-
-        screenSize = 1024
-        requireLevel = (start - end) / screenSize;
+    def getDownsampleData(self, group, start, end , screenSize = 1024):
+       
+        bestLevel = self.getBestLevel(group, start , end, screenSize)
         
-        return false #not yet implemented
-
+        data = self.getData(group,bestLevel)
+        print "data has been downsampled "+ str(bestLevel) + " times"
+        return data[start:end]
+            
     def getLevels(self, group):
 
         group = self.hdf['/'+group]
-        return group.name
+        ilist = group.items()
+        levels = list()
+
+        for index in range(len(ilist)):
+            levels.append(int(ilist[index][0]))
+        
+        return levels
+    
+    def getBestLevel(self, group, start, end , screenSize = 1024):
+        
+        requireLevel = (end - start) / screenSize;
+        possibleLevels = self.getLevels(group)
+        
+        min = abs(requireLevel - possibleLevels[0])
+        bestFit = 0
+        for i in range(1,len(possibleLevels)):
+            if (min > abs(requireLevel - possibleLevels[i])):
+                min = abs(requireLevel - possibleLevels[i])
+                bestFit = i        
+       
+        
+        return possibleLevels[bestFit]
+    
+    def getGroupLength(self,group): #not yet implemented
+        return 1200000
+    
+    def binary_search(self, a, x, lo=0, hi=None): 
+        x = x/1000
+        hi = hi if hi is not None else len(a)  
+        return bisect_left(a,x,lo,hi) 
