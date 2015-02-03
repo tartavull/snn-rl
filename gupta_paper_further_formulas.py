Corrected scoring reporting now outputs 87.5% classification with dirac scaling and lat inh at -18mv
abs and rel refrac set specifically too.  Last upload was of the wrong file

gupta_paper_further_formulas.py
@@ -55,11 +55,11 @@ class gupta_paper:
 
 	correctSpikes = np.array([[1]*(totalSpikeIntervals+1)]*dictionaryLongitude);#[[1] * totalSpikeIntervals]*dictionaryLongitude
 	correctSpikes[0][0] = 0
-	correctSpikes[0][4:13] = 0
+	correctSpikes[0][4:12] = 0
 	correctSpikes[1][0:4] = 0
-	correctSpikes[1][7:13] = 0
+	correctSpikes[1][7:12] = 0
 	correctSpikes[2][0:7] = 0
-	correctSpikes[2][10:13] = 0
+	correctSpikes[2][10:12] = 0
 	correctSpikes[3][0:10] = 0
     
 	M = None
@@ -70,16 +70,12 @@ class gupta_paper:
 	testRelRefracTimeActivated = [False]*dictionaryLongitude	
 	testAbsRefracTimeActivated = [False]*dictionaryLongitude
 	# Parameters for tuning
-	relRefracTimeDuration = 0.03#.1#.01		
-	absRefracTimeDuration = 0.02#.02#.002	
-	#dendCalcScaling = 0.16#.32#1.0
-	dendCalcScaling = 1.0
-	#somaDirectScaling = .0005#.0005#.001#.0005#.001#0.002285#0.002857
-	somaDirectScaling = 1.0
-	diracScaling = 0.1#0.5#1.0#0.1#1.0#0.0007#0.0007#0.0825#0.0325#0.0825
-	#diracScaling = 1.0
+	relRefracTimeDuration = 0.02#.1#.01		
+	absRefracTimeDuration = 0.01#.02#.002	
+	diracCalcScaling = 0.0#.32#1.0
+	somaDirectScaling = .001#.0005#.001#0.002285#0.002857
 	#uMScaling = 1.0#.32
-	lateralInhibitScaling = -18*mV#18*mV#-13*mV#17*mV#12.5*mV#25*mV#8.0*mV
+	lateralInhibitScaling = -13*mV#17*mV#12.5*mV#25*mV#8.0*mV
 
 	print 'initial Weights\n',W
 
@@ -470,25 +466,17 @@ class gupta_paper:
 					
 					# simplify dirac until later.  TODO: try more comple dirac
 					#if (t > -(Dt/2) and t < (Dt/2)):
-					# Seems to me what is looked for here is that the post synapse (output neurons) is after the pre synapse (input neurons)
 					if Dt >= 0.0:
 						if self.time == 0.121 or self.time == 0.421 or self.time == 0.721 or self.time == 1.021:
 							DiracFun = 1.00
 						if Dt != 0: 
-							# A closer post to pre synapse creates a larger dirac signal for greater weight I'd suppose than
-							# a further distance below in DiracFun
-							#Dt = Dt*1000
 							DiracFun = 1/Dt
 						else:
-							DiracFun = 1#1000
-						#DiracFun = 1
-						dendGroup[IdIndex] = float(DiracFun)*volt*.001*self.diracScaling #add .001 as scaling factor.  
-						# Scaling factor of .001 could account for 1ms being present instead of .001 as it is computed, difference of
-						# 1/1 compared to 1/.001
-						#dendGroup[IdIndex] = float(DiracFun)*volt*self.diracScaling
+							DiracFun = 1000
+						dendGroup[IdIndex] = float(DiracFun)*volt*.001 #add .001 as scaling factor
 					else:
 						DiracFun = 0
-						dendGroup[IdIndex] = float(DiracFun)*volt*.001*self.diracScaling						
+						dendGroup[IdIndex] = float(DiracFun)*volt*.001						
 
 				return dendGroup				
 
@@ -559,7 +547,7 @@ class gupta_paper:
 					for neuronIndex in range(dictionaryLongitude):
 						# dend calcs
 						for indexOfDend in range(dictionaryLongitude):
-							testADDS.DendI[indexOfDend] = sum(testDend[indexOfDend].v[:])*self.dendCalcScaling
+							testADDS.DendI[indexOfDend] = sum(testDend[indexOfDend].v[:])*self.diracCalcScaling
 						# somaDirect calcs
 						ADDS.somaDirectCalcs(neuronIndex, testADDS, testSomaDirect, testDend)
 
@@ -567,7 +555,7 @@ class gupta_paper:
 
 						#checkForResets(neuronIndex)
 
-					printEvalOutputForTesting()
+					#printEvalOutputForTesting()
 
 				def checkForResets(neuronIndex):
 					if self.v2[neuronIndex] == 10*mV:
@@ -613,8 +601,6 @@ class gupta_paper:
 					print 'testDend.dirac','time',self.testTime,'\t',testDend[0].dirac,testDend[1].dirac,testDend[2].dirac,testDend[3].dirac
 					print 'testDend.v',testDend[0].v,testDend[1].v,testDend[2].v,testDend[3].v
 					print 'sum(testDend[neuronIndex])',sum(testDend[0].v[:]),sum(testDend[1].v[:]),sum(testDend[2].v[:]),sum(testDend[3].v[:])							
-					print 'testSomaDirect.v',testSomaDirect.v[0],testSomaDirect.v[1],testSomaDirect.v[2],testSomaDirect.v[3]
-					print 'sum(testSomaDirect[neuronIndex])',sum(testSomaDirect.v[0]),sum(testSomaDirect.v[1]),sum(testSomaDirect.v[2]),sum(testSomaDirect.v[3])							
 					print 'testADDS.v',testADDS.v[0],testADDS.v[1],testADDS.v[2],testADDS.v[3]
 					#print 'Prelim Weights\n0',testDend[0].w[:]
 					#print '1',testDend[1].w[:]