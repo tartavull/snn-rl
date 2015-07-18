d7a08ccb733072b2e5c1d32a0c881f775451dddb
Nate Sutton <natemsutton@gmail.com>
2015-07-17 22:18:30 -0400

    Added runtime profiler progs, profileProg, to find how to run the sim faster

--- profileProg3.py
+++ profileProg3.py
@@ -1,11 +1,29 @@
+'''
+Profiling sim to find ways to quicken run time
+reference: https://zapier.com/engineering/profiling-python-boss/
+'''
 from hyperopt import fmin, tpe, hp
 import gupta_paper_further_formulas as snnrl
 import sys
+import cProfile
+
+def do_cprofile(func):
+    def profiled_func(*args, **kwargs):
+        profile = cProfile.Profile()
+        try:
+            profile.enable()
+            result = func(*args, **kwargs)
+            profile.disable()
+            return result
+        finally:
+            profile.print_stats()
+    return profiled_func
 
 def snnrlWithParams(*args, **kwargs):
-	sys.argv = ['snnrl.py', args, kwargs]
-	return snnrl.main()	
+    sys.argv = ['snnrl.py', args, kwargs]
+    return snnrl.main() 
 
+@do_cprofile
 def objective(args):
     minRand, maxRand, posReinfVal, negReinfVal = args
     # train
@@ -17,11 +35,13 @@ def objective(args):
 
     return {'loss': accuracyPerc}
 
+#objective([0.6, 0.9, 1.5, 3.0])    
+
 space = [hp.uniform('minRand', 0.6, 0.7), hp.uniform('maxRand', 0.9, 0.9), hp.uniform('posReinfVal', 1.0, 5.0), hp.uniform('negReinfVal', 1.0, 5.0)]
 
 best = fmin(objective,
     space,
     algo=tpe.suggest,
-    max_evals=5)#100
+    max_evals=3)#100
 
 print best
